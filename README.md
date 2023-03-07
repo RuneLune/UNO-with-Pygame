0c3c5333fe7455f1b81ace1f652a0a69b8dd503a
Kyle S. Park <teardrops@kakao.com>
2023-03-07 14:17:31 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# UNO-with-Pygame
+UNO-with-Pygame
+===============
+![Generic badge](https://img.shields.io/badge/Build-pending-lightgray.svg)
+![Generic badge](https://img.shields.io/badge/License-MIT-orange.svg)
+![Generic badge](https://img.shields.io/badge/Python-3.9-blue.svg)
+![Generic badge](https://img.shields.io/badge/Linter-Flake8-skyblue.svg)
+![Generic badge](https://img.shields.io/badge/Format-Black-black.svg)
+![Generic badge](https://img.shields.io/badge/IDE-VSCode-blue.svg)
+
 UNO with Pygame Project from Software Engineering class. 
+
+# [UNO Game Card Assets](https://opengameart.org/content/uno)
+
+UNO game card images are sourced from [opengameart.org](https://opengameart.org/) created by the author [JotaFaD](https://opengameart.org/users/jotafad). 
---
+++ src/app.py
@@ -0,0 +1,34 @@
+import pygame
+import sys
+from pygame.locals import QUIT
+
+# Variables
+width = 600
+height = 400
+white = (255, 255, 255)
+black = (0, 0, 0)
+fps = 30
+
+# Pyhame initialize
+pygame.init()
+
+pygame.display.set_caption("Hello, world!")
+displaysurf = pygame.display.set_mode((width, height), 0, 32)
+clock = pygame.time.Clock()
+
+gulimfont = pygame.font.SysFont("arial", 70)
+helloworld = gulimfont.render("Hello, world!", 1, black)
+hellorect = helloworld.get_rect()
+hellorect.center = (width / 2, height / 2)
+
+# Pygame main
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    displaysurf.fill(white)
+    displaysurf.blit(helloworld, hellorect)
+
+    pygame.display.update()
+    clock.tick(fps)
