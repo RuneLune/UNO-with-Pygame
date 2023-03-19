497805a2c3f83cb6afd4d3ca13f62711f8394d28
yonguk <kimyu9827@naver.com>
2023-03-19 22:23:40 +0900

    mapping func update

--- src/keymap.py
+++ src/keymap.py
@@ -1,13 +1,9 @@
 import pygame
 
-pygame.init()
-win = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
-rect = pygame.Rect(0, 0, 40, 60)
-rect.center = win.get_rect().center
-vel = 50
-
-key = {"up": 1, "down": 1, "left": 0, "right": 0}
+key_saved = {"up": 1073741906, "down": 1073741905,
+             "left": 1073741904, "right": 1073741903}  # default value is arrow
+key_default = {"up": 1073741906, "down": 1073741905,
+               "left": 1073741904, "right": 1073741903}
 
 
 class KeyMap():
@@ -15,90 +11,78 @@ class KeyMap():
         self.keys = pygame.key.get_pressed()
         self.is_custom = False
 
-    def key_custom_up(self):
-        print(1)
-        pygame.event.wait(5000)
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.TEXTINPUT:
-                print(2)
-                key["up"] = pygame.key.key_code(event.text)
-            else:
-                print(3)
+    def key_custom(self, target):
+        pygame.event.wait()
+        event = pygame.event.poll()
+        if event.type == pygame.TEXTINPUT:
+            key_saved[target] = pygame.key.key_code(event.text)
+        elif event.type == pygame.KEYDOWN and key_default[target]:
+            key_saved[target] = key_default[target]
 
-    def key_custom_down(self):
-        print(1)
-        pygame.event.wait(5000)
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.TEXTINPUT:
-                print(2)
-                key["down"] = pygame.key.key_code(event.text)
-            else:
-                print(3)
+    def down(self):
+        if self.is_custom == True:
+            return self.keys[key_saved["down"]]
+        else:
+            return self.keys[pygame.K_DOWN]
 
-    def key_custom_left(self):
-        if pygame.event.type == pygame.KEYDOWN:
-            key["left"] = pygame.key.key_code(pygame.event.key)
-
-    def key_custom_right(self):
-        if pygame.event.type == pygame.KEYDOWN:
-            key["right"] = pygame.key.key_code(pygame.event.key)
+    def up(self):
+        if self.is_custom == True:
+            return self.keys[key_saved["up"]]
+        else:
+            return self.keys[pygame.K_UP]
 
     def left(self):
         if self.is_custom == True:
-            return self.keys[key["left"]]
+            return self.keys[key_saved["left"]]
         else:
             return self.keys[pygame.K_LEFT]
 
     def right(self):
         if self.is_custom == True:
-            return self.keys[key["right"]]
+            return self.keys[key_saved["right"]]
         else:
             return self.keys[pygame.K_RIGHT]
 
-    def down(self):
-        if self.is_custom == True:
-            print(chr(key["down"]))
-            return self.keys[pygame.key.key_code(chr(key["down"]))]
-        else:
-            print(self.keys[pygame.K_DOWN])
-            return self.keys[pygame.key.key_code("down")]
 
-    def up(self):
-        if self.is_custom == True:
-            return self.keys[pygame.key.key_code(chr(key["up"]))]
-        else:
-            return self.keys[pygame.key.key_code("up")]
+##########################
+# key mapping test code
 
+# pygame.init()
+# win = pygame.display.set_mode((800, 600))
+# clock = pygame.time.Clock()
+# rect = pygame.Rect(0, 0, 40, 60)
+# rect.center = win.get_rect().center
+# vel = 50
 
-run = True
-custom = True
+# custom = True
+# run = True
+# while run:
+#     clock.tick(10)
 
-while run:
-    clock.tick(3)
+#     key_map = KeyMap()
+#     key_map.is_custom = True
 
-    key_map = KeyMap()
-    key_map.is_custom = True
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if custom == True:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    key_map.key_custom_up()
-                elif event.key == pygame.K_DOWN:
-                    key_map.key_custom_down()
-                    custom = False
+#         if event.type == pygame.KEYDOWN and custom == True:
+#             if pygame.key.name(event.key) == "up":
+#                 key_map.key_custom("up")
+#             elif pygame.key.name(event.key) == "down":
+#                 key_map.key_custom("down")
+#             elif pygame.key.name(event.key) == "left":
+#                 key_map.key_custom("left")
+#             elif pygame.key.name(event.key) == "right":
+#                 key_map.key_custom("right")
+#             elif pygame.key.name(event.key) == "enter":
+#                 custom = False
 
-    print("key:", key)
+#     rect.x += (key_map.right() - key_map.left()) * vel
+#     rect.y += (key_map.down() - key_map.up()) * vel
 
-    rect.x += (key_map.right() - key_map.left()) * vel
-    rect.y += (key_map.down() - key_map.up()) * vel
+#     win.fill((0, 0, 0))
+#     pygame.draw.rect(win, (255, 0, 0), rect)
+#     pygame.display.update()
 
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), rect)
-    pygame.display.update()
-
-pygame.quit()
+# pygame.quit()
