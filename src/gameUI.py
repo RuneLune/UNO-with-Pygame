4d31a1ad818070ef97aa5e12e07ded0681043741
yonguk <kimyu9827@naver.com>
2023-03-31 20:57:10 +0900

    user,bot card image draw added

--- src/gameUI.py
+++ src/gameUI.py
@@ -26,12 +26,11 @@ class Game_UI:
         self.cards.refresh()
 
         self.screen_size = settings.get_screen_resolution()
-        print(self.screen_size)
         self.screen = pygame.display.set_mode(self.screen_size)
         self.surface = pygame.Surface(self.screen_size)
 
-        self.card_size = self.cards.get_card_image(100).get_rect().size
-
+        self.card_size = self.cards.get_card_image(000).get_rect().size
+        self.card_back_image = self.cards.get_card_image(000)
         self.render()
 
     def render(self):
@@ -42,23 +41,23 @@ class Game_UI:
         user_space_size = (self.screen_size[0]*(3/4), self.screen_size[1]*(1/3))
         self.user_space_pos = (0,self.screen_size[1]*(2/3))
 
-        bots_space_size = (self.screen_size[0]*(1/4), self.screen_size[1]*(1/5))
-        self.bots_space_pos = [(user_space_size[0],i * bots_space_size[1]) for i in range(len(self.players))]
+        self.bots_space_size = (self.screen_size[0]*(1/4), self.screen_size[1]*(1/5))
+        self.bots_space_pos = [(user_space_size[0],i * self.bots_space_size[1]) for i in range(len(self.players))]
 
         # space rectangular definition
         self.deck_space = pygame.Rect(deck_space_pos, deck_space_size)
         self.user_space = pygame.Rect(self.user_space_pos, user_space_size)
-        self.bots_space = [pygame.Rect(self.bots_space_pos[i], bots_space_size) for i in range(len(self.players))]
+        self.bots_space = [pygame.Rect(self.bots_space_pos[i], self.bots_space_size) for i in range(len(self.players))]
 
         # font for user, bot name
         self.font = pygame.font.Font("res/font/Travel.ttf", 20)
         self.user_name_text = self.font.render("insert_User_name", True, colors.white)
-        self.bot_name_text =[self.font.render("computer " + str(i), True, colors.white) for i in range(len(self.players))]
+        self.bot_name_text =[self.font.render("computer " + str(i), True, colors.white) for i in range(len(self.bots))]
 
         self.user_card_center_pos = (self.user_space.centerx - self.card_size[0]/2,
                                      self.user_space.centery - self.card_size[1]/2 )# user space 중앙 좌표
         self.bot_card_center_pos = [(self.bots_space[i].centerx - self.card_size[0]/2,
-                                    self.bots_space[i].centery - self.card_size[1]/2) for i in range(len(self.players))]
+                                    self.bots_space[i].centery - self.card_size[1]/2) for i in range(len(self.bots))]
 
     def refresh(self, player_count):
         self.players = self.game.get_players()
@@ -82,14 +81,13 @@ class Game_UI:
         pygame.draw.rect(self.surface,colors.black,rect=self.user_space,border_radius=1)
         self.screen.blit(self.user_name_text, self.user_space_pos)
 
-        for i in range(len(self.players)):
+        for i in range(len(self.bots)):
             pygame.draw.rect(self.surface,colors.red,self.bots_space[i],border_radius=1)
             self.screen.blit(self.bot_name_text[i],self.bots_space_pos[i])
         
         # draw card space
         user_card_list = self.user.get_hand_cards()
         user_card_num = len(user_card_list)
-        bot_card_num = [len(self.bots[i].get_hand_cards()) for i in range(len(self.bots))]
 
         # 플레이어가 가지고 있는 카드 이미지 로드
         user_card_image = [self.cards.get_card_image(num) for num in user_card_list]
@@ -105,7 +103,18 @@ class Game_UI:
                                                      self.user_card_center_pos[1]))
 
         # 봇의 카드그리기
-
+        for i in range(len(self.bots)):
+            bot_card_num = len(self.bots[i].get_hand_cards())
+            if bot_card_num%2 == 0:
+                for j in range(bot_card_num):
+                    self.screen.blit(self.card_back_image,(self.bot_card_center_pos[i][0] + self.card_size[0]*(j-bot_card_num/2)//2,
+                                                            self.bot_card_center_pos[i][1] + self.bots_space_size[1]*i))
+            else:
+                for j in range(bot_card_num):
+                    self.screen.blit(self.card_back_image,(self.bot_card_center_pos[i][0] + self.card_size[0]*(j-bot_card_num//2)//2,
+                                                            self.bot_card_center_pos[i][1] + self.bots_space_size[1]*i))
+            
+                
     def __darw_pause_menu(self):
         pass
 
