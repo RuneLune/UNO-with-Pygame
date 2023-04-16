d5b4c9bebfc5d3a190aeda1a8beac2b47bf0fcd2
imjm <77835002+imjm@users.noreply.github.com>
2023-04-16 15:31:03 +0900

    스테이지 시작 확인 창 띄우기 구현

--- src/stage_select.py
+++ src/stage_select.py
@@ -6,6 +6,7 @@ class Stage:
     def __init__(self, settings):
         self.__stage_num = 4
         self.__settings = settings
+        self.__is_confirm = False
         self.refresh()
 
         return super().__init__()
@@ -20,6 +21,7 @@ class Stage:
         self.__title_font = pygame.font.Font("res/font/MainFont.ttf", round(screen_size[1] / 6))
         self.__back_font = pygame.font.Font("res/font/MainFont.ttf", round(screen_size[1] / 20))
         self.__imformation_font = pygame.font.Font("res/font/MainFont.ttf", round(screen_size[1] / 30))
+        self.__window_font = pygame.font.Font("res/font/MainFont.ttf", round(screen_size[1] / 15))
 
         # title
         self.__title_text = self.__title_font.render("STAGE", True, colors.white)
@@ -34,10 +36,8 @@ class Stage:
         self.__button_rect[0].bottom = self.__screen.get_rect().centery / 5
 
         # 게임 정보
-        self.__imformation_text.append(self.__imformation_font.render("첫분배에 컴퓨터가 기술 카드를 50% 더 높은 확률로 받음", True, colors.white))
-        self.__imformation_text.append(self.__imformation_font.render("3명의 컴퓨터 플레이어와 대전, 카드는 같은 수로 분배 (첫 카드 제외)", True, colors.white))
-        self.__imformation_text.append(self.__imformation_font.render("2명의 컴퓨터 플레이어와 대전, 매 5턴마다 낼 수 있는 카드 색 무작위 변경", True, colors.white))
-        self.__imformation_text.append(self.__imformation_font.render("Stage D", True, colors.white))
+        for i in range(len(self.__imformation)):
+            self.__imformation_text.append(self.__imformation_font.render(self.__imformation[i], True, colors.white))
         self.__imformation_rect = self.__imformation_text[-1].get_rect()
         self.__imformation_rect.top = self.__screen.get_rect().centery + 70
         self.__imformation_rect.left = self.__screen.get_rect().right/10
@@ -61,16 +61,35 @@ class Stage:
                 self.__button_rect[i+1].left =  self.__screen.get_rect().right/2
             elif i == 3:
                 self.__button_rect[i+1].right =  self.__screen.get_rect().right/1.2
-                
-        # 하이라이팅
-        self.__selected_rect = pygame.Rect(0, 0, 130, 130)
+
+        # window
+        self.__window_text = self.__window_font.render("Do you want to start?", True, colors.white)
+        self.__window_text_rect = self.__window_text.get_rect()
+        self.__window_text_rect.top = self.__screen.get_rect().centery + 110
+        self.__window_text_rect.left = self.__screen.get_rect().right/3.2
+
+        self.__window_img.append(pygame.image.load(f"res/img/stage/yes.png"))
+        self.__window_img.append(pygame.image.load(f"res/img/stage/no.png"))
+        self.__window_rect.append(self.__window_img[0].get_rect())
+        self.__window_rect.append(self.__window_img[1].get_rect())
+        for i in range(len(self.__window_rect)):
+            self.__window_rect[i].top = self.__screen.get_rect().centery + 200
+            if i == 0:
+                self.__window_rect[i].left = self.__screen.get_rect().right/10
+            elif i == 1:
+                self.__window_rect[i].right = self.__screen.get_rect().right/1.2
+
+        self.__background = pygame.Surface((self.__screen.get_width(), self.__screen.get_height()))
+        self.__background.fill((255, 255, 255))  
+        self.__background.set_alpha(128) 
 
         
-            
-            
+                
+        # 하이라이트
+        self.__selected_rect = pygame.Rect(0, 0, 130, 130)
+        self.__selected_window_rect = pygame.Rect(0, 0, 130, 110)
 
-
-    
+        
     def refresh(self):
         pygame.display.set_caption("STAGE")
         flag = 0
@@ -80,10 +99,19 @@ class Stage:
         self.__button_text = []
         self.__button_rect = []
         self.__stage_img = []
+        self.__window_img = []
         self.__imformation_text = []
+        self.__imformation = [
+            "첫분배에 컴퓨터가 기술 카드를 50% 더 높은 확률로 받음",
+            "3명의 컴퓨터 플레이어와 대전, 카드는 같은 수로 분배 (첫 카드 제외)",
+            "2명의 컴퓨터 플레이어와 대전, 매 5턴마다 낼 수 있는 카드 색 무작위 변경",
+            "미정"
+        ]
         # 첫번쨰 인덱스는 back 버튼을 위한 것. 2~5가 스테이지를 위한 엘리먼트들
         self.__touchable = [True, True, True, True, True]
+        self.__window_rect = []
         self.__selected_stage = 0
+        self.__selected_window = 0
         self.render()
         return None
     
@@ -111,49 +139,87 @@ class Stage:
             # 화면에 선택된 메뉴 옵션에 흰색 테두리를 그림
             pygame.draw.rect(self.__screen, colors.white, selected_rect, 2)
             self.__screen.blit(self.__imformation_text[self.__selected_stage - 1], self.__imformation_rect)
+
+        if self.__is_confirm:
+            self.draw_window()
         return None
+    
+    def draw_window(self):
+
+        self.__screen.blit(self.__background, (0, 0))
+
+        self.__screen.blit(self.__window_text, self.__window_text_rect)
+
+        for i, img in enumerate(self.__window_img):
+            self.__screen.blit(img, self.__window_rect[i])
+
+
+        selected_window_rect = pygame.Rect(0, 0, self.__selected_window_rect.width, self.__selected_window_rect.height)
+        selected_window_rect.center = self.__window_rect[self.__selected_window].center
+        pygame.draw.rect(self.__screen, colors.white, selected_window_rect, 2)
+        
 
         
             
     def handle(self, event):
-        if event.type == pygame.MOUSEMOTION:
-            mouse_pos = pygame.mouse.get_pos()
-            for i in range(len(self.__button_rect)):
-                if self.__touchable[i] and self.__button_rect[i].collidepoint(mouse_pos):
-                    self.__selected_stage = i
-                    continue
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            for i in range(len(self.__button_rect)):
-                if self.__touchable[i] and self.__button_rect[i].collidepoint(mouse_pos):
-                    return self.__menu_func(i)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                self.__selected_stage -= 1
-                if self.__selected_stage < 0:
-                    self.__selected_stage = len(self.__button_rect) - 1
-                while not self.__touchable[self.__selected_stage]:
+        if self.__is_confirm == False:
+            if event.type == pygame.MOUSEMOTION:
+                mouse_pos = pygame.mouse.get_pos()
+                for i in range(len(self.__button_rect)):
+                    if self.__touchable[i] and self.__button_rect[i].collidepoint(mouse_pos):
+                        self.__selected_stage = i
+                        continue
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                for i in range(len(self.__button_rect)):
+                    if self.__touchable[i] and self.__button_rect[i].collidepoint(mouse_pos):
+                        return self.__menu_func(i)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
                     self.__selected_stage -= 1
                     if self.__selected_stage < 0:
                         self.__selected_stage = len(self.__button_rect) - 1
-            elif event.key == pygame.K_RIGHT:
-                self.__selected_stage += 1
-                if self.__selected_stage >= len(self.__button_rect):
-                    self.__selected_stage = 0
-                while not self.__touchable[self.__selected_stage]:
+                    while not self.__touchable[self.__selected_stage]:
+                        self.__selected_stage -= 1
+                        if self.__selected_stage < 0:
+                            self.__selected_stage = len(self.__button_rect) - 1
+                elif event.key == pygame.K_RIGHT:
                     self.__selected_stage += 1
                     if self.__selected_stage >= len(self.__button_rect):
                         self.__selected_stage = 0
-            elif event.key == pygame.K_RETURN:
-                return self.__menu_func(self.__selected_stage)
-            elif event.key == pygame.K_ESCAPE:
-                return pygame.event.post(pygame.event.Event(events.CHANGE_SCENE, target="main"))
+                    while not self.__touchable[self.__selected_stage]:
+                        self.__selected_stage += 1
+                        if self.__selected_stage >= len(self.__button_rect):
+                            self.__selected_stage = 0
+                elif event.key == pygame.K_RETURN:
+                    return self.__menu_func(self.__selected_stage)
+        else:
+            self.__window_handle(event, self.__selected_stage)
 
-    
+    def __window_handle(self, event, idx):
+        if event.type == pygame.MOUSEMOTION:
+            mouse_pos = pygame.mouse.get_pos()
+            for i in range(len(self.__window_rect)):
+                if self.__window_rect[i].collidepoint(mouse_pos):
+                    self.__selected_window = i
+                    continue
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            for i in range(len(self.__window_rect)):
+                if self.__window_rect[i].collidepoint(mouse_pos):
+                    return self.__window_func(i, idx)
+
     def __menu_func(self, i):
         if i == 0:
             self.__settings.get_real_settings().update(previous_scene="stage")
             return pygame.event.post(pygame.event.Event(events.CHANGE_SCENE, target="main"))
-        elif i == 1:
+        elif i == 1 or i == 2 or i == 3 or i == 4:
+            self.__is_confirm = True
             pass
-            
+    
+    def __window_func(self, i, idx):
+        if i == 0:
+            # 각 스테이지로 이동 idx 1부터 4까지 a~d 이동
+            pass
+        else:
+            self.__is_confirm = False
