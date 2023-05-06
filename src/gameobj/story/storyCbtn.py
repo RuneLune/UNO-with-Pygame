3be9aa0ec96dda424cedee6fe3c0ea3d4cae7341
imjm <77835002+imjm@users.noreply.github.com>
2023-05-06 16:32:43 +0900

    마우스 올렸을 시 블러처리

--- src/gameobj/story/storyCbtn.py
+++ src/gameobj/story/storyCbtn.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from overrides import overrides
 import pygame
 from os.path import join
+import cv2
+import numpy
 
 from gameobj.txtobj import TextObject
 from gameobj.gameobj import GameObject
@@ -25,6 +27,7 @@ class StoryCButton(GameObject):
             self.image = pygame.image.load(image_resource(
                 join("stage", "story_0.png")))
         self.rect = self.image.get_rect()
+        self.img_copy = self.image
         self.z_index = 997
         
         return None
@@ -32,14 +35,29 @@ class StoryCButton(GameObject):
     @overrides
     def on_mouse_enter(self) -> None:
         if self.touchable[2]:
+            self.image = self.create_neon(self.image)
             self.story_c_text.visible()
         return None
 
     @overrides
     def on_mouse_exit(self) -> None:
         if self.touchable[2]:
+            self.image = self.img_copy
             self.story_c_text.invisible()
         return None
 
+    def create_neon(self, surf):
+        surf_alpha = surf.convert_alpha()
+        rgb = pygame.surfarray.array3d(surf_alpha)
+        alpha = pygame.surfarray.array_alpha(surf_alpha).reshape((*rgb.shape[:2], 1))
+        image = numpy.concatenate((rgb, alpha), 2)
+        cv2.GaussianBlur(image, ksize=(9, 9), sigmaX=10, sigmaY=10, dst=image)
+        cv2.blur(image, ksize=(5, 5), dst=image)
+        bloom_surf = pygame.image.frombuffer(
+            image.flatten(), image.shape[1::-1], "RGBA"
+        )
+        bloom_surf = pygame.transform.rotate(bloom_surf, 270)
+        bloom_surf = pygame.transform.flip(bloom_surf, True, False)
+        return bloom_surf
 
     pass
