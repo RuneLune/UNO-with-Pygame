bcaf3bd10d08d01b0ce638f1904f2bd581028364
Kyle S. Park <teardrops@kakao.com>
2023-05-09 17:48:57 +0900

    Refactoring on mainmenu

---
+++ src/gameobj/mainmenu/menu.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from overrides import overrides
+from typing import Optional, TYPE_CHECKING
+import pygame
+
+from ..txtbtnobj import TextButtonObject
+from util.resource_manager import font_resource
+import util.colors as color
+
+if TYPE_CHECKING:
+    from manager.scenemgr import SceneManager
+
+
+class Menu(TextButtonObject):
+    Inst_created: int = 0
+    Insts: list[Menu] = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.Inst_created += 1
+        return super().__new__(cls)
+
+    @overrides
+    def start(self) -> None:
+        Menu.Insts.append(self)
+        screen_rect = pygame.display.get_surface().get_rect()
+        self.font = pygame.font.Font(
+            font_resource("MainFont.ttf"), screen_rect.height // 12
+        )
+        self.color = color.black
+        self.image = self.font.render(self.text, True, self.color)
+        self.rect = self.image.get_rect()
+        self.center = (
+            screen_rect.centerx,
+            screen_rect.height * (6 + self.Inst_created) // 12,
+        )
+        self._target_scene: Optional[str] = None
+
+    def attach_mgr(self, scene_manager: SceneManager) -> Menu:
+        self.scene_manager = scene_manager
+        return self
+
+    @overrides
+    def on_click(self) -> None:
+        self.scene_manager.load_scene(self.target_scene)
+        return None
+
+    @property
+    def target_scene(self) -> str:
+        return self._target_scene
+
+    @target_scene.setter
+    def target_scene(self, value: str) -> None:
+        self._target_scene = value
+        return None
+
+    @overrides
+    def on_destroy(self) -> None:
+        Menu.Insts.remove(self)
+        Menu.Inst_created -= 1
+        return None
+
+    @staticmethod
+    def destroy_all() -> None:
+        for inst in Menu.Insts:
+            inst.on_destroy()
+            del inst
+            pass
+        Menu.Inst_created = 0
+        return None
+
+    pass
--- src/scene/mainmenu.py
+++ src/scene/mainmenu.py
@@ -1,95 +1,38 @@
 from overrides import overrides
 import pygame
 
-from util.resource_manager import font_resource
 import util.colors as color
 from .scene import Scene
-from gameobj.gameobj import GameObject
-from gameobj.txtobj import TextObject
-from gameobj.txtbtnobj import TextButtonObject
+from gameobj.bgobj import BackgroundObject
+from gameobj.mainmenu.titletxt import TitleText
+from gameobj.mainmenu.menu import Menu
 
 
 class MainMenu(Scene):
     @overrides
     def start(self) -> None:
-        screen_rect = pygame.display.get_surface().get_rect()
-        background_surface = pygame.Surface(screen_rect.size)
-        background_surface.fill(color.white)
-        title_font = pygame.font.Font(
-            font_resource("MainFont.ttf"), screen_rect.height // 3
-        )
-        menu_font = pygame.font.Font(
-            font_resource("MainFont.ttf"), screen_rect.height // 12
-        )
+        self.play_button = Menu("Play").attach_mgr(self.scene_manager)
+        self.stage_button = Menu("Stage").attach_mgr(self.scene_manager)
+        self.achievements_button = Menu("Achievements").attach_mgr(self.scene_manager)
+        self.settings_button = Menu("Settings").attach_mgr(self.scene_manager)
+        self.quit_button = Menu("Quit").attach_mgr(self.scene_manager)
 
-        self.background = GameObject(
-            background_surface, "MainMenu_Background", z_index=-999
-        )
-        self.title_text = TextObject(
-            "UNO", title_font, color.black, "MainMenu_TitleText"
-        )
-        self.play_button = TextButtonObject(
-            "Play", menu_font, color.black, "MainMenu_PlayButton"
-        )
-        self.stage_button = TextButtonObject(
-            "Stage", menu_font, color.black, "MainMenu_StageButton"
-        )
+        self.play_button.target_scene = "game_scene"
+        self.stage_button.target_scene = "story_scene"
+        self.achievements_button.target_scene = "achievements_scene"
+        self.settings_button.target_scene = "config_menu"
+        self.quit_button.target_scene = "quit_scene"
 
-        self.achive_button = TextButtonObject(
-            "Achivement", menu_font, color.black, "MainMenu_StageButton"
-        )
-
-        self.settings_button = TextButtonObject(
-            "Settings", menu_font, color.black, "MainMenu_SettingsButton"
-        )
-        self.quit_button = TextButtonObject(
-            "Quit", menu_font, color.black, "QuitButton"
-        )
-
-        self.title_text.rect.center = (
-            screen_rect.centerx,
-            screen_rect.height * 2 // 10,
-        )
-        self.play_button.rect.center = (
-            screen_rect.centerx,
-            screen_rect.height * 5 // 10,
-        )
-        self.stage_button.rect.center = (
-            screen_rect.centerx,
-            screen_rect.height * 6 // 10,
-        )
-        self.achive_button.rect.center = (
-            screen_rect.centerx,
-            screen_rect.height * 7 // 10,
-        )
-        self.settings_button.rect.center = (
-            screen_rect.centerx,
-            screen_rect.height * 8 // 10,
-        )
-        self.quit_button.rect.center = (
-            screen_rect.centerx,
-            screen_rect.height * 9 // 10,
-        )
-
-        self.play_button.on_mouse_up_as_button = lambda: self.scene_manager.load_scene(
-            "game_scene"
-        )
-        self.stage_button.on_mouse_up_as_button = lambda: self.scene_manager.load_scene(
-            "story_scene"
-        )
-        self.settings_button.on_mouse_up_as_button = (
-            lambda: self.scene_manager.load_scene("config_menu")
-        )
         self.quit_button.on_mouse_up_as_button = lambda: pygame.event.post(
             pygame.event.Event(pygame.QUIT)
         )
 
-        self.instantiate(self.background)
-        self.instantiate(self.title_text)
+        self.instantiate(BackgroundObject(color.white))
+        self.instantiate(TitleText("UNO"))
         self.instantiate(self.play_button)
         self.instantiate(self.stage_button)
+        self.instantiate(self.achievements_button)
         self.instantiate(self.settings_button)
         self.instantiate(self.quit_button)
-        self.instantiate(self.achive_button)
 
         return None
