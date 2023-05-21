d6d303e08ac10fcd6a7e3c06ec7dbac5e36416a0
Kyle S. Park <teardrops@kakao.com>
2023-05-21 11:00:23 +0900

    Fixed SceneManager

--- src/manager/scenemgr.py
+++ src/manager/scenemgr.py
@@ -32,37 +32,53 @@ class SceneManager(metaclass=SingletonMeta):
             "create_server": CreateServer,
             "quit": QuitScene,
         }
-        self.current_scene_name = "main_menu"
+        self.scene_name_list = ["quit", "main_menu"]
         self.current_scene = self.scenes[self.current_scene_name](self)
-        self.previous_scene_name = "quit"
-        self.previous_scene = self.scenes[self.previous_scene_name](self)
+        return None
 
     def load_scene(self, scene_name: str) -> None:
-        self.previous_scene_name = self.current_scene_name
-        self.current_scene_name = scene_name
-        self.current_scene.exit()
-        del self.current_scene
-        pygame.display.get_surface().fill((0, 0, 0))
-        self.current_scene = self.scenes[self.current_scene_name](self)
-        if self.previous_scene_name == "gamelobby" and self.current_scene_name == "game_scene":
+        self.scene_name_list.append(scene_name)
+        self._load_scene()
+        if (
+            self.previous_scene_name == "gamelobby"
+            and self.current_scene_name == "game_scene"
+        ):
             self.current_scene.start()
             pass
         return None
 
-    def reload_scene(self) -> None:
+    def _load_scene(self) -> None:
         self.current_scene.exit()
         del self.current_scene
         pygame.display.get_surface().fill((0, 0, 0))
         self.current_scene = self.scenes[self.current_scene_name](self)
         return None
 
+    def reload_scene(self) -> None:
+        return self._load_scene()
+
     def load_previous_scene(self) -> None:
-        return self.load_scene(self.previous_scene_name)
+        self.scene_name_list.pop()
+        self._load_scene()
 
     def handle_scene(self, event: pygame.event.Event) -> None:
         return self.current_scene.handle(event)
 
     def update_scene(self) -> None:
         return self.current_scene.tick()
+
+    @property
+    def previous_scene_name(self) -> str:
+        if len(self.scene_name_list) >= 2:
+            return self.scene_name_list[-2]
+        else:
+            return "quit"
+
+    @property
+    def current_scene_name(self) -> str:
+        if self.scene_name_list:
+            return self.scene_name_list[-1]
+        else:
+            return "quit"
 
     pass
